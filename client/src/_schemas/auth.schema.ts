17ef221fa86d6cd4bf714435daf8d2ff43030c1b
Maxime <maxime.huynh@okolo.fr>
2025-05-29 15:21:18 +0200

    add error message

--- client/src/_schemas/auth.schema.ts
+++ client/src/_schemas/auth.schema.ts
@@ -2,8 +2,12 @@ import { z } from 'zod';
 
 
 export const Authschema = z.object({
-  username: z.string().min(2).max(50),
-  password: z.string().min(8).max(50)
+  username: z.string()
+    .min(2, "Le nom d'utilisateur doit contenir au moins 2 caractères")
+    .max(30, "Le nom d'utilisateur doit contenir moins de 30 caractères"),
+  password: z.string()
+    .min(8, "Le mot de passe doit contenir au moins 8 caractères")
+    .max(30, "Le mot de passe doit contenir moins de 30 caractères")
 })
 
 export type AuthRequest = z.infer<typeof Authschema>;
